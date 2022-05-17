36704659fd6834509d91661fbabd1f40f3a72736
signekp <sign4080@gmail.com>
2022-05-17 18:51:06 +0200

    Portfolio

--- src/components/FixedIcons.js
+++ src/components/FixedIcons.js
@@ -1,12 +1,25 @@
-import React from 'react'
+import React from "react";
 
 export default function FixedIcons() {
-    return (
-        <div className="icon-cointainer">
-            <a href="https://www.linkedin.com/in/signe-petersen-0b7452162/"
-                target="_blank" rel="noreferrer" className='linkedIn-icon' alt="picture"> <i className="fa-brands fa-linkedin-in"></i></a>
-            <a href="https://github.com/signekp"
-                target="_blank" rel="noreferrer" className='github-icon'><i className="fa-brands fa-github"></i></a>
-        </div>
-    )
+  return (
+    <div className="icon-cointainer">
+      <a
+        href="mailto:signekp@outlook"
+        target="_blank"
+        rel="noreferrer"
+        className="linkedIn-icon"
+        alt="picture"
+      >
+        <i className="fa-solid fa-envelope"></i>
+      </a>
+      <a
+        href="https://github.com/signekp"
+        target="_blank"
+        rel="noreferrer"
+        className="github-icon"
+      >
+        <i className="fa-brands fa-github"></i>
+      </a>
+    </div>
+  );
 }
--- src/components/ProjectElement.js
+++ src/components/ProjectElement.js
@@ -1,16 +1,18 @@
-import React from 'react'
+import React from "react";
 
 export default function ProjectElement(props) {
-    return (
-
-        <div className="projectElement">
-
-            <a href={props.item.link} target="_blank" rel="noreferrer">
-                <img src={`../images/${props.item.coverImg}`} alt="project" className="card-img" />
-                <h3 className='projectHeader project-padding'>{props.item.title}</h3>
-                <p className="project-description">{props.item.shortDescription}</p>
-                <p className="project-lang project-padding">{props.item.language}</p>
-            </a>
-        </div>
-    )
+  return (
+    <div className="projectElement">
+      <a href={props.item.link} target="_blank" rel="noreferrer">
+        <img
+          src={`./images/${props.item.coverImg}`}
+          alt="project"
+          className="card-img"
+        />
+        <h3 className="projectHeader project-padding">{props.item.title}</h3>
+        <p className="project-description">{props.item.shortDescription}</p>
+        <p className="project-lang project-padding">{props.item.language}</p>
+      </a>
+    </div>
+  );
 }
