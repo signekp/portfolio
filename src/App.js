f4371c74466c098801728e10bb222e721a3b6725
signekp <sign4080@gmail.com>
2022-05-17 19:24:29 +0200

    Your awesome message

--- src/App.js
+++ src/App.js
@@ -1,44 +1,29 @@
-import './App.css';
-import './App-mid.css';
-import FixedIcons from './components/FixedIcons';
-import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import About from './pages/About';
-import Home from './pages/Home'
-import Footer from './components/Footer';
-import Projects from './pages/Projects';
-import Contact from './pages/Contact';
+import "./App.css";
+import "./App-mid.css";
+import FixedIcons from "./components/FixedIcons";
+import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
 
-
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Footer from "./components/Footer";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <div className="container">
-      <Router>
-        <Navbar />
+      <Navbar />
 
-        <FixedIcons />
+      <FixedIcons />
 
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-
-          <Route path="/projects">
-            <Projects />
-          </Route>
-
-          <Route path="/contact">
-            <Contact />
-          </Route>
-
-          <Route path="/">
-            <Home />
-          </Route>
-
-        </Switch>
-        <Footer />
-      </Router>
+      <Routes>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/projects" element={<Projects />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+      <Footer />
     </div>
   );
 }
